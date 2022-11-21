91c366c15f5069e67cd9e773abc1f67cbffc2654
Kenrick-Zhou <Kenrick.Zhou@gmail.com>
2022-11-21 12:30:34 +0800

    fix: 解决不在项目目录下启动程序就找不到config.yml文件的问题 refactor: 添加log.path_dir配置，提取日志目录初始化工作，而不是每个日志配置都单独检查创建 feat: 服务启动和每次循环检查后都打印info日志，便于排查问题

--- updated.py
+++ updated.py
@@ -1,9 +1,11 @@
 import time
 
 from update import update
-from init import CFG_RDS_A
+from init import CFG_RDS_A, loginfo
 
 if __name__ == '__main__':
+    loginfo('service started.')
     while True:
         update(CFG_RDS_A['DBInstanceIds'], CFG_RDS_A['ArrayName'])
         time.sleep(CFG_RDS_A['Interval'])
+        loginfo('one loop over')
